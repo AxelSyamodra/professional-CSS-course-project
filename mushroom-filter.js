300402b3c4c66a765e4752096b3a742aabd17842
Syamodra <codesyamodra@gmail.com>
2025-07-09 11:06:25 +0700

    Updating the filter
    Make the filter shows the correct cards including when show: all selected

--- mushroom-filter.js
+++ mushroom-filter.js
@@ -14,4 +14,27 @@ edibleFilter.addEventListener("change", updateFilter);
 function updateFilter(e) {
   const filterType = e.target.name;
   currentFilters[filterType] = e.target.value;
+
+  filterCards();
+}
+
+function filterCards() {
+  cards.forEach((card) => {
+    // .getAttribute same as .dataset if using data []
+    const season = card.querySelector("[data-season]").dataset.season;
+    const edible = card.querySelector("[data-edible]").dataset.edible;
+
+    const matchesSeason = currentFilters.season === season;
+    const matchesEdible = currentFilters.edible === edible;
+
+    // to filter and make 'all' shows up
+    if (
+      (currentFilters.season === "all" || currentFilters.season === season) &&
+      (currentFilters.edible === "all" || currentFilters.edible === edible)
+    ) {
+      card.hidden = false;
+    } else {
+      card.hidden = true;
+    }
+  });
 }
