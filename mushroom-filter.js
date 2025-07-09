fd56a6d8da46d40471064d7e65d9eb04d7cb0c15
Syamodra <codesyamodra@gmail.com>
2025-07-09 10:41:38 +0700

    Start working on the filter
    Create mushroom-filter.js to handle the filtering function. Add id to the mushroom-guide.html select to make the filter work

---
+++ mushroom-filter.js
@@ -0,0 +1,15 @@
+const cards = document.querySelectorAll(".mushroom-guide .card");
+
+const seasonalFilter = document.querySelector("#season");
+const edibleFilter = document.querySelector("#edible");
+
+const currentFilters = {
+  season: "all",
+  edible: "all",
+};
+
+seasonalFilter.addEventListener("change", (event) => {
+  currentFilters.season = event.target.value;
+
+  console.log(currentFilters);
+});
